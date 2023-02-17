b2d2aa4ab36ce46fa97765fdfb56b33955282c31
PicEncrypt Project <98749433+picencrypt@users.noreply.github.com>
2023-02-17 23:33:15 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+const { app, BrowserWindow } = require('electron')
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 400,
+    height: 800,
+    icon:'./build/icons/default.png',
+    title:"PicEncrypt",
+    frame: false,
+resizable: false,
+transparent: true,
+  })
+  
+  win.loadFile('app.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+})
+const electron = require('electron')
+
+const Menu = electron.Menu
+
+Menu.setApplicationMenu(null);
